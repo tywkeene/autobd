2469af7e73ef6bd37ac4dd7f19458d4036e8d71b
Tyrell Keene <tyrell.wkeene@gmail.com>
2015-05-16 14:02:37 -0500

    Still fighting with build.sh...

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -v -a -ldflags "-X main.commit $(git rev-parse --short=10 HEAD)" ./
+go build -v -a -ldflags "-X main.commit $(git rev-parse --short=10 HEAD)" github.com/SaviorPhoenix/autobd
