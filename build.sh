fa650423387469a6a30f8fb64a1abffabceba045
Tyrell Keene <tyrell.wkeene@gmail.com>
2015-05-27 16:11:23 -0500

    Migrate to the gb project management tool. Migrate to using the recommended gb directory structure. (Builds will still work using go build in the src directory)

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -v -a -ldflags "-X main.commit $(git rev-parse --short=10 HEAD)" github.com/SaviorPhoenix/autobd
+gb build -a -ldflags "-X main.commit $(git rev-parse --short=10 HEAD)" github.com/SaviorPhoenix/autobd
