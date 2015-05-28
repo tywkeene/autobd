e3288672c7ae9cbf294884bbe9f9905f5dbdea15
Tyrell Keene <tyrell.wkeene@gmail.com>
2015-05-28 15:50:18 -0500

    Added toml configuration support, added config.toml

--- main.go
+++ main.go
@@ -19,12 +19,12 @@ func init() {
 }
 
 func main() {
-	if err := syscall.Chroot(*options.Flags.Root); err != nil {
+	if err := syscall.Chroot(options.Config.Root); err != nil {
 		panic("chroot: " + err.Error())
 	}
-	if err := os.Chdir(*options.Flags.Root); err != nil {
+	if err := os.Chdir(options.Config.Root); err != nil {
 		panic(err)
 	}
-	log.Printf("Serving '%s' on port %s", *options.Flags.Root, *options.Flags.ApiPort)
-	log.Panic(http.ListenAndServe(":"+*options.Flags.ApiPort, nil))
+	log.Printf("Serving '%s' on port %s", options.Config.Root, options.Config.ApiPort)
+	log.Panic(http.ListenAndServe(":"+options.Config.ApiPort, nil))
 }
--- options/options.go
+++ options/options.go
@@ -3,32 +3,44 @@ package options
 import (
 	"flag"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"os"
 )
 
-type Options struct {
-	Root    *string
-	ApiPort *string
-	Node    *bool
-	Seed    *string
+type Conf struct {
+	Root    string `toml:"root_dir"`
+	ApiPort string `toml:"api_port"`
+	Node    bool   `toml:"run_as_node"`
+	Seed    string `toml:"seed_server"`
 }
 
-var Flags *Options
+var Config Conf
 
 func GetOptions() {
-	Flags = &Options{}
-	Flags.Root = flag.String("root", "", "Root directory to serve (required)")
-	Flags.ApiPort = flag.String("api-port", "8081", "Port that the API listens on")
-	Flags.Node = flag.Bool("node", false, "Run as a node")
-	Flags.Seed = flag.String("seed", "", "Seed server to connect with (required when running as a node)")
+	var configFile string
+
+	flag.StringVar(&configFile, "config", "", "Configuration file")
+	flag.StringVar(&Config.Root, "root", "", "Root directory to serve (required)")
+	flag.StringVar(&Config.ApiPort, "api-port", "8081", "Port that the API listens on")
+	flag.BoolVar(&Config.Node, "node", false, "Run as a node")
+	flag.StringVar(&Config.Seed, "seed", "", "Seed server to connect with (required when running as a node)")
+
 	flag.Parse()
 
-	if *Flags.Root == "" {
-		fmt.Println("-root argument required")
+	if configFile != "" {
+		if _, err := toml.DecodeFile(configFile, &Config); err != nil {
+			fmt.Printf("Error reading config %s: %s\n", configFile, err.Error())
+			os.Exit(-1)
+		}
+		fmt.Printf("Configration file options in %s overriding command line options\n", configFile)
+	}
+
+	if Config.Root == "" {
+		fmt.Println("Must specify root directory")
 		os.Exit(-1)
 	}
 
-	if *Flags.Node == true && *Flags.Seed == "" {
+	if Config.Node == true && Config.Seed == "" {
 		fmt.Println("Must specify seed server when running as node")
 		os.Exit(-1)
 	}
