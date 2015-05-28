c491787786ddc475d136b015cb8e718b568536dc
Tyrell Keene <tyrell.wkeene@gmail.com>
2015-05-28 16:25:29 -0500

    Fixed dirty tree detection in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$(git diff-files --quiet)" != 0 ]; then
+git diff-files --quiet
+if [ "$?" != 0 ]; then
     COMMIT="$(git rev-parse --short=10 HEAD)-dirty"
 else
     COMMIT="$(git rev-parse --short=10 HEAD)"
