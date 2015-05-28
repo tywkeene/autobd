62c8bd48a307d479eb5d280cd241fd97a4c0aa90
Tyrell Keene <tyrell.wkeene@gmail.com>
2015-05-28 16:09:20 -0500

    Added dirty git tree detection to build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-go build -v -a -ldflags "-X main.commit $(git rev-parse --short=10 HEAD)" github.com/tywkeene/autobd
+
+if [ "$(git diff-files --quiet)" != 0 ]; then
+    COMMIT="$(git rev-parse --short=10 HEAD)-dirty"
+else
+    COMMIT="$(git rev-parse --short=10 HEAD)"
+fi
+
+go build -v -a -ldflags "-X main.commit $COMMIT" github.com/tywkeene/autobd
