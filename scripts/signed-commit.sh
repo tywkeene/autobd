4ee30f22eff23048f5706ce734a3ca6c461c627e
Tyrell Keene <tyrell.wkeene@gmail.com>
2017-02-04 00:29:19 -0700

    Add scripts/signed-commit.sh

---
+++ scripts/signed-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Sign and commit. Won't work if you dont have gpg/git/github set up
+# to do this. See: https://help.github.com/articles/signing-commits-using-gpg/
+
+if [ -z "$1" ]; then
+    echo "Refusing to commit without a message"
+    exit 1
+fi
+
+git commit -a -S -m "$1"
